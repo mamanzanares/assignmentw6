479ff2a13138f857f034fe2e8af69f2ae791c628
Miguel Angel Manzanares <ma.manzanaresgut@gmail.com>
2022-12-01 11:25:33 +0100

    Edited test create opportunity to check if a a contact was autocreated

--- src/test/java/com/assignment/assignment/OpportunityTest.java
+++ src/test/java/com/assignment/assignment/OpportunityTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.assignment.assignment.dao.ContactRepository;
 import com.assignment.assignment.dao.OpportunityRepository;
 import com.assignment.assignment.model.Contact;
 import com.assignment.assignment.model.Opportunity;
@@ -30,14 +32,32 @@ class OpportunityTest {
 	@Autowired
 	OpportunityService opportunityService;
 	
+	@Autowired
+	ContactRepository contactRepository;
+	
 	
 	@Test
 	@Order(1)
 	void createOpportunity() {
+		//Creating the opportunity,saving it and getting it from db
 		Opportunity opportunity = new Opportunity("test3", "Pedro", null, null,true);
 		opportunityService.createOpportunity(opportunity);
 		Optional<Opportunity> tmpOpportunity = opportunityRepository.findById("test3");
+		//Looking in the list of contacts one with the opportunity id of the one that was created
+		Iterable<Contact> tmpContact = contactRepository.findAll();
+		List<Contact> tmpList = new ArrayList<>();
+		tmpContact.forEach(tmpList::add);
+		Iterator it = tmpList.iterator();
+		boolean find = false;
+		while(it.hasNext()) {
+			Contact contact = (Contact)it.next();
+			if(contact.getOpportunity().getId().equals(opportunity.getId())) {
+				find=true;
+			}
+		}
+		
 		assertTrue(tmpOpportunity.isPresent());
+		assertTrue(find);
 	}
 	
 	
