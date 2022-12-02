c52875b628ee381d60f6424f407b9d1e48f36d97
Miguel Angel Manzanares <ma.manzanaresgut@gmail.com>
2022-12-02 14:43:53 +0100

    Solution connection with front

--- src/main/java/com/assignment/assignment/controller/LoginController.java
+++ src/main/java/com/assignment/assignment/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.assignment.assignment.model.User;
 import com.assignment.assignment.service.LoginService;
 
 @RestController
+@CrossOrigin
 public class LoginController {
 
 	@Autowired
--- src/main/java/com/assignment/assignment/controller/OpportunityController.java
+++ src/main/java/com/assignment/assignment/controller/OpportunityController.java
@@ -3,6 +3,7 @@ package com.assignment.assignment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.assignment.assignment.model.Opportunity;
 import com.assignment.assignment.service.OpportunityService;
 
 @RestController
+@CrossOrigin
 public class OpportunityController {
 
 	@Autowired
